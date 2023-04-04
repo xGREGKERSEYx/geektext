39c32f8d0d1c08645190dc01f0fbf50b2c615a3e
CycliOMG <124520049+CycliOMG@users.noreply.github.com>
2023-04-03 21:00:12 -0400

    Add files via upload

---
+++ BookDetails/src/main/java/Author.java
@@ -0,0 +1,57 @@
+
+
+public class Author {
+    private String firstName;
+    private String lastName;
+    private String biography;
+    private String publisher;
+    private String authorId;
+    
+      public Author(String firstName, String lastName, String biography, String publisher, String authorId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.biography = biography;
+        this.publisher = publisher;
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+}
